a03d98c2aeb5408dcd4e2a300a05ad2048ee1b2a
bg2345 <bgardynski@gmail.com>
2019-12-05 02:47:26 -0500

    first commit

---
+++ src/auth/config.js
@@ -0,0 +1,8 @@
+
+export default {
+  clientId: "PR1PQVbhe9ilDr1o3mqV41S2sLJujs9d",
+  domain: "dev-nmq6gvrw.auth0.com",
+  redirect: "http://localhost:3000/close-popup",
+  logoutUrl: "http://localhost:3000",
+  audience: "https://react-messenger-api"
+};
